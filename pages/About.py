e3bfbd1c91b0d99191766b56dc42d1c238695481
BH0HB <bagb54458@gmail.com>
2025-05-14 14:30:28 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("This app is under construction.")
 
-url="https://github.com/MK316/streamlit25/raw/main/images/image00.png"
+url="https://github.com/BH0HB/streamlit25/raw/main/images/IMG_0041.jpeg"
 st.image(url, caption="Image link", width=300)  # width in pixels
 
