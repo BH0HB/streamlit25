20ef88a50aa909784c5dcc64d6b3fc3739282737
BH0HB <bagb54458@gmail.com>
2025-05-14 14:18:19 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,7 +1,7 @@
 import streamlit as st
  
 st.title("🥑 Group 1")
-st.caption("This project means that I wanna go home.")
+st.caption("Princess' diary")
 
 st.write("This should be a collaboratory work")
 st.markdown("---")
